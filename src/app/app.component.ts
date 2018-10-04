e70d7f009f6e458cda249a19236411cc7f81095a
Vawi <val.winnen@gmail.com>
2018-10-04 16:13:41 +0200

    Mise en place activité 1 angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'JDIJZIAJOQSJAINSA',
+      loveIts: 2
+    },
+    {
+      title: 'Mon deuxieme post',
+      content: 'JDIJZIAJOQSJAINSAJDOS£ajisnalindvz',
+      loveIts: 0
+    },
+    {
+      title: 'Encore un post',
+      content: 'JDIJZIAJOQSJAINSAOSAI£diajcajcdjnjijsdzjashfoahcpsjiapcsnpcnapeafn',
+      loveIts: 1
+    }
+  ];
+
+}
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;   
+  @Input() loveIts: number;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getColor() {
+    if(this.loveIts > 0) {
+      return 'green';
+    } else if(this.loveIts < 1) {
+      return 'red';
+    }
+}
+
+}
